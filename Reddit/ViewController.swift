68c31389e481965c6bc20f58330ad182153c510a
Valeriia Zakharova <zakh.valeriia@gmail.com>
2020-07-21 16:55:40 +0300

    initial commit

---
+++ Reddit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reddit
+//
+//  Created by Valeriia Zakharova on 21.07.2020.
+//  Copyright © 2020 Valeriia Zakharova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
