3cab6a3edb631aaa7040e91866898ab30b0a47c3
Valeriia Zakharova <zakh.valeriia@gmail.com>
2020-10-07 21:12:02 +0300

    PostModel has been refactored. PostCell was improved. Added extra fields.

--- Reddit/PostsListViewController.swift
+++ Reddit/PostsListViewController.swift
@@ -12,12 +12,40 @@ class PostsListViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var service: PostsService = PostsServiceImpl()
+    
+    var posts: [PostModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         tableView.register(UINib(nibName: Constants.cellIndetifier, bundle: nil), forCellReuseIdentifier: Constants.cellIndetifier)
+        
+        getPosts()
     }
+   
+    //MARK: - Private
+    
+    private func getPosts() {
+        service.fetchPosts { [weak self] (posts, error) in
+            guard let self = self else { return }
+            if let error = error {
+                self.showError(error.localizedDescription)
+                print(error)
+            } else {
+                self.posts = posts
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func showError(_ error: String) {
+        let alertController = UIAlertController()
+        alertController.message = error
+        alertController.addAction(.init(title: "OK", style: .cancel, handler: nil))
+        present(alertController, animated: true)
+    }
+    
 }
 
   //MARK: - UITableViewDataSourse
@@ -25,11 +53,33 @@ class PostsListViewController: UIViewController {
 extension PostsListViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIndetifier) as! PostCell
+        let post = posts[indexPath.row]
+        cell.authorLabel.text = post.author
+        cell.groupnameLabel.text = post.subreddit
+        cell.commentsCountLabel.text = String(post.comentsAmount) //= (post.comentsAmount.flatMap {String($0)} ) ?? ""
+        cell.likeCountLabel.text = String(post.score)//= post.score.flatMap({ (score) -> String in
+            //String(score)
+        //}) ?? ""
+        cell.titleLabel.text = post.title
+        
+        if let url = URL(string: post.thumbnail) {
+            DispatchQueue.global().async {
+                do {
+                    let data = try Data(contentsOf: url)
+                    let image = UIImage(data: data)
+                    DispatchQueue.main.async {
+                        cell.contentImageView.image = image
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+        }
         
         return cell
     }
--- Reddit/Services/Model/PostModel.swift
+++ Reddit/Services/Model/PostModel.swift
@@ -8,9 +8,33 @@
 
 import Foundation
 
-struct PostModel: Codable {
-    let id: String
-    let title: String
+struct PostModel: Decodable {
+    let subreddit: String
     let author: String
-    let thumbnail: URL
+    let score: Int //?
+    let title: String
+    let comentsAmount: Int
+    let thumbnail: String //?
+    let url: String
+    
+    enum CodingKeys: String, CodingKey {
+        case subreddit
+        case author
+        case score
+        case title
+        case comentsAmount = "num_comments"
+        case thumbnail
+        case url
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        author = try container.decode(String.self, forKey: .author)
+        comentsAmount = try container.decode(Int.self, forKey: .comentsAmount)
+        subreddit = try container.decode(String.self, forKey: .subreddit)
+        score = try container.decode(Int.self, forKey: .score)
+        title = try container.decode(String.self, forKey: .title)
+        thumbnail = try container.decode(String.self, forKey: .thumbnail)
+        url = try container.decode(String.self, forKey: .url)
+    }
 }
